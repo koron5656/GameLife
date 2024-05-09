f85c827cb7582b2739a11dbfdf4c2995f114156d
EC2 Default User <ec2-user@ip-172-31-21-88.ap-northeast-1.compute.internal>
2024-05-09 14:04:31 +0000

    投稿フォーム作成途中

--- app/controllers/postimages_controller.rb
+++ app/controllers/postimages_controller.rb
@@ -1,11 +1,25 @@
 class PostimagesController < ApplicationController
   def new
+    @post = Post.new
   end
 
+    # 投稿データの保存
+  def create
+    @post = Post.new(post_image_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_images_path
+  end
   def index
   end
 
   def show
   end
 
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+    # 投稿データの保存
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path
+  end
+  def index
+  end
+
+  def show
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
 end
