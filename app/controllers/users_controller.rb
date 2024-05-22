346ff1c14ae3c1c9fa9d3e3acb2d79e7f52d3274
EC2 Default User <ec2-user@ip-172-31-21-88.ap-northeast-1.compute.internal>
2024-05-22 10:11:07 +0000

    [Finish]15章【ユーザーに関する機能(2) - ユーザーページの作成】

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,37 @@
 class UsersController < ApplicationController
-    before_action :authenticate_admin!
-    def destroy
-        @user = User.find(params[:id])
-        @user.destroy
-        redirect_to admin_dashboards_path, notice: 'ユーザーを削除しました。'
+    # before_action :authenticate_admin!
+
+  def show
+    @user = User.find(params[:id])
+    @post = @user.post_images
+    # @post = @user.post_images.page(params[:page])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
     end
+  end
+    # def destroy
+    #     @user = User.find(params[:id])
+    #     @user.destroy
+    #     redirect_to admin_dashboards_path, notice: 'ユーザーを削除しました。'
+    # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
 
+  has_one_attached :profile_image
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
